f20045c036eacee616aaa95d98163759ebfacfec
Олжас Садвокасов <@local.itstep>
2018-11-20 21:29:44 +0600

    Добавьте файлы проекта.

---
+++ Project5/Source.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+
+
+
+
+
+	/*int a, b, x;
+
+	printf("¬ведите число первой переменной:");
+	scanf_s("%d", &a);
+
+	printf("¬ведите число первой переменной:");
+	scanf_s("%d", &b);
+
+
+
+	x = a;
+	a = b;
+	b = x;
+
+	printf("b=%d\n", b);
+	printf("a=%d\n", a);
+	printf("x=%d\n", x);*/
+
+	
+	
+
+
+	/*int a, b, c;
+
+	printf("¬ведите количество жителей =");
+	scanf_s("%d\n", &a);
+
+	printf("¬ведите площадь = ");
+	scanf_s("%d", &b);
+
+	c = b / a;
+	printf("%d\n", &c);*/
+
+
+
+	/*int m, v, r;
+
+	printf("¬ведите массу = ");
+	scanf_s("%d", &m);
+	printf("¬ведите объем = ");
+	scanf_s("%d\n", &v);
+
+	r = m / v;
+	printf("%d\n", &r);*/
+
+
+	/*float z, x, y, a, b;
+
+	printf("¬ведите x=");
+	scanf_s("%f", &x);
+	printf("¬ведите y=");
+	scanf_s("%f", &y);
+
+	a = x * x * x;
+	b = x * x;
+	z = a - (2, 5 * x*y) + (1, 78 * x*x) - (2, 5 * y) + 1;
+
+	printf("%f\n", &z);*/
+
+
+
+	/*int a, p;
+
+	a = 4;
+
+	p = 4 * a;
+
+	printf("%d\n", p);*/
+
+
+	/*int y, x, a;
+
+	printf("¬ведите x=");
+	scanf_s("%d\n", &x);
+
+	a = x * x;
+
+	y = (7 * a) - (3 * (x + 6));
+
+	printf("y=");
+	scanf_s("%d", &y);*/
+	
+
+	/*printf("5, 10 \n");
+	printf("7см \n");
+	printf("100 t \n, 1949 v \n");
+	printf("x25 \n xy");*/
+
+	system("pause");
+
+}
